0d15ecf501092eb16b6899be21cbd7bbb3b57379
Martijn van Dorp <martijnvandorp@casema.nl>
2015-08-11 00:13:09 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#######################################################################
+### EDIT THESE PATHS FOR YOUR OWN SETUP                             ###
+#######################################################################
+
+# The left4dead2 directory of your server
+L4D2_DIR="/home/steam/Steam/steamapps/common/Left 4 Dead 2 Dedicated Server/left4dead2/"
+
+#######################################################################
+### EDIT THESE IF YOU WANT TO INSTALL ONLY SOME OF THE ADDONS       ###
+#######################################################################
+
+main() {
+    install "mmsource"
+    install "sourcemod"
+    install "stripper"
+
+    install "tickrate-enabler"
+    install "dhooks"
+    install "left4downtown2"
+    install "l4dtoolz"
+    install "builtinvotes"
+    install "geoip"
+    install "cannounce"
+    install "confogl"
+    install "promod"
+    install "various"
+}
+
+#######################################################################
+### SHOULD NOT HAVE TO EDIT BELOW HERE                              ###
+#######################################################################
+
+install() {
+    # Create a temporary directory
+    dir=$(mktemp -d)
+
+    # Extract the addon to the temp directory
+    tar zxvf "${1}.tar.gz" -C "$dir"
+
+    # Merge the temp directory into the server's left4dead2 folder
+    rsync -av "$dir/" "${L4D2_DIR}"
+
+    # Clean up the temp directory
+    rm -rf "$dir"
+}
+
+main
